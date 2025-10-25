e9052bf7299f1cc2b2af396a6503559eb12051ee
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2025-10-25 13:19:44 +0530

    update cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173", "https://food-reels.netlify.app"],
     credentials: true
 }))
 app.use(express.json())
