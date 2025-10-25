392822d1c7cf2622abd5e6979fd1a378e00bda26
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2025-10-25 14:49:59 +0530

    update auth

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -30,7 +30,7 @@ export const register = async (req, res) => {
         res.cookie("token", token,{
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         })
 
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
         res.cookie("token", token,{
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000 // 1 day
         })
 
